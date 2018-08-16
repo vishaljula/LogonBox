eb7fa75f718aeac71846fd0e232dae438949e968
Aditya Vishal <vishal.jula@gmail.com>
2018-08-15 21:34:59 -0400

    Added functionality to encrypt password

--- server/server.js
+++ server/server.js
@@ -2,7 +2,27 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');1
 var jwt = require('jwt-simple');
+var bcrypt = require('bcrypt-nodejs');
 var app = express();
+
+// do a get request to get the user name and pwd
+var database = {
+	loginData: {
+		userName: 'mounika',
+		pwd: 'admin'
+	},
+	details: {
+		ipAddr: '192.168.0.1',
+		subnet: '255.255.255.0',
+		networkGateway: '192.168.1.1'
+	}
+};
+
+//
+bcrypt.hash(database.loginData.pwd, null, null, (err, hash) => {
+	database.loginData.pwd = hash;
+});
+//`
 
 var foo = 'dgchvd';
 
@@ -14,35 +34,24 @@ app.get('/posts', (req, res) => {
 });
 
 app.get('/details', (req, res) => {
-	// get the details from db
-	var details = {
-		ipAddr: '192.168.0.1',
-		subnet: '255.255.255.0',
-		networkGateway: '192.168.1.1'
-	};
-
-	res.send(details);
+	res.send(database.details);
 });
 
 app.post('/login', (req, res) => {
 	var userData = req.body;
-	// do a get request to get the user name and pwd
-	var userFromDb = {
-		userName: 'mounika',
-		pwd: 'admin'
-	};
 
-	if(!userFromDb || userData.pwd !== userFromDb.pwd) {
-		return res.status(401).send({message: 'Email or Password invalid'});
-	}
+	bcrypt.compare(userData.pwd, database.loginData.pwd, (err, matched) => {
+		if(!matched) {
+			return res.status(401).send({message: 'Email or Password invalid'});
+		}
+		var payload = {};
 
-	var payload = {};
+		var token = jwt.encode(payload, '123');
 
-	var token = jwt.encode(payload, '123');
+		console.log(token);
 
-	console.log(token);
-
-	res.status(200).send({token: token});
+		res.status(200).send({token: token});
+	});
 });
 
 app.listen(3000);
